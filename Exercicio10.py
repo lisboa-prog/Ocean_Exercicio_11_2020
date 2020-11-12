8a5a97d311a01fc83c9434fd7b06fdbd0a3e5376
David-Lisboa <dlrlisboa@gmail.com>
2020-11-11 23:57:07 -0400

    Exercicio10.py concluido

--- Exercicio10.py
+++ Exercicio10.py
@@ -7,7 +7,19 @@ Dificuldade: Principiante
 
 1 - Escreva um programa que receba o nome de uma pessoa;
 2 - Extraia a primeira letra do nome;
-3 - Compare a primeira letra de modo que, caso seja "A", exiba na tela "A de Amor", caso seja "B", exiba na tela "B de Baixinho", caso contrário, exiba na tela "Seu nome não é legal.";
+3 - Compare a primeira letra de modo que, caso seja "A", exiba na tela "A de Amor", caso seja "B", exiba na tela
+    "B de Baixinho", caso contrário, exiba na tela "Seu nome não é legal.";
 4 - Faça com que o programa funcione independe se for digitado um nome com letras minúsculas ou maiúsculas.
 """
 
+def Exercico10():
+    nome = str(input("\nDigite o nome:")).lower()
+
+    if nome[0] == 'a':
+        print("\nA de Amor")
+    elif nome[0] == 'b':
+        print("\nB de Baixinho")
+    else:
+        print("\nSeu nome não é legal.")
+
+Exercico10()
