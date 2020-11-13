f919267ed4c571b7ca661f1fcf5dc47b8a26520f
David-Lisboa <dlrlisboa@gmail.com>
2020-11-12 22:01:22 -0400

    Exercicio17.py concluido

--- Exercicio17.py
+++ Exercicio17.py
@@ -1,7 +1,20 @@
 ﻿"""
 Exercício 17
+
 Nome: Par ou Ímpar: Funções
-Objetivo: Escrever uma função chamada numero_par que recebe um número e torna True caso ele seja par ou False caso seja ímpar
+Objetivo: Escrever uma função chamada numero_par que recebe um número e torna True caso ele seja par ou False caso
+        seja ímpar
 Dificuldade: Principiante
-Crie uma função chamada 'numero_par(numero)' que receba um número como argumento e retorne True caso ele seja par ou False caso seja ímpar
+
+Crie uma função chamada 'numero_par(numero)' que receba um número como argumento e retorne True caso ele seja par ou
+    False caso seja ímpar
 """
+
+def numero_par(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+
+numero = float(input("Digite um numero: "))
+print(numero_par(numero))
