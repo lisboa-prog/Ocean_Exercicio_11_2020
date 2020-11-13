2f67bb67b6f899bff6f0940f1b5e760f8e24184f
David-Lisboa <dlrlisboa@gmail.com>
2020-11-12 21:44:44 -0400

    Exercicio16.py concluido

--- Exercicio16.py
+++ Exercicio16.py
@@ -1,15 +1,38 @@
 """
 Exercício 16
+
 Nome: Joguinho: Salão
-Objetivo: Escrever uma função que funciona como um jogo, onde o usuário deve digitar as suas escolhas e o programa irá indicar o resultado das escolhas.
+Objetivo: Escrever uma função que funciona como um jogo, onde o usuário deve digitar as suas escolhas e o programa irá
+    indicar o resultado das escolhas.
 Dificuldade: Intermediário
+
 Crie uma função principal chamada jogo() e execute as tarefas abaixo:
 1 - O jogador irá entrar em um salão (informe isso a ele utilizando o comando print())
 2 - Você deve perguntar se ele quer escolher entre a porta esquerda ou a direita
 3 - Se a resposta for "esquerda" ou "e", exiba a mensagem:
-- Você entrou na sala à esquerda, parece que não tem nada por aqui. Acho melhor você voltar correndo!
+    - Você entrou na sala à esquerda, parece que não tem nada por aqui. Acho melhor você voltar correndo!
 4 - Caso contrário, se a resposta for "direita" ou "d", exiba a mensagem:
-- Excelente escolha! A sala à direita estava te esperando esse tempo todo! Que maravilhoso isso. Pena que não tem nada aqui para você. :(
-5 - Caso contrário, se a resposta não foi nenhum dos valores anteriores, exiba a mensagem abaixo e execute a função 'jogo()' novamente.
-- Você não escolheu nenhuma das portas. Tente novamente.
+    - Excelente escolha! A sala à direita estava te esperando esse tempo todo! Que maravilhoso isso. Pena que não tem
+    nada aqui para você. :(
+5 - Caso contrário, se a resposta não foi nenhum dos valores anteriores, exiba a mensagem abaixo e execute a função
+    'jogo()' novamente.
+    - Você não escolheu nenhuma das portas. Tente novamente.
 """
+
+
+def jogo():
+    print(
+        "\nVocê acobou de entrar em salão.\nExistem 2 portas uma a esquerda e outra a direita\nDigite d - direita ou e - esquerda")
+    escolha = str(input("\nDigite a sua escolha: ")).lower()
+
+    if escolha == 'e' or escolha == 'esquerda':
+        print("\nVocê entrou na sala à esquerda, parece que não tem nada por aqui. Acho melhor você voltar correndo!")
+    elif escolha == 'd' or escolha == 'direita':
+        print("\nExcelente escolha! A sala à direita estava te esperando esse tempo todo! Que maravilhoso isso."
+              " Pena que não tem nada aqui para você. :( ")
+    else:
+        print("\nVocê não escolheu nenhuma das portas. Tente novamente.")
+        jogo()
+
+
+jogo()
